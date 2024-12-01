ec7c5512f1295cec142c1e0189e8d03111305b9c
Nikki Miguel <102607255+NatsokoMiruku@users.noreply.github.com>
2024-12-01 23:55:29 +0800

    import csv functions into db

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import { CentralNodeGameInformation, Node2GameInformation, Node3GameInformation } from '../DBConn.js';
+import csvParser from 'csv-parser';
+import fs from 'fs';
+import { parse } from 'date-fns';
+
 
 const transactionsRouter = express.Router();
 
-transactionsRouter.get('/', (req,res) => {
+/*transactionsRouter.get('/', (req,res) => {
   res.render('index');
-});
+});*/
 
 // read all of the records
-transactionsRouter.get('/gamelists', async (req, res) => {
+transactionsRouter.get('/', async (req, res) => {
   try {
     // limit the display of the steamgames records to 20
-    const steamgames = await CentralNodeGameInformation.findAll({ limit: 20 });
+    const steamgames = await CentralNodeGameInformation.findAll({ limit: 20, raw: true });
+    console.log(steamgames);
 
     res.render('index', { games: steamgames });
   } catch (error) {
@@ -113,5 +118,134 @@ transactionsRouter.post('/gamelists/delete/:id', async (req, res) => {
     res.status(500).send('Error deleting record: ' + error.message);
   }
 });
+
+
+//import csv
+/*transactionsRouter.get('/importcsv', async (req, res) => {
+  console.log('CSV IMPORT CALLED');
+  const csvFilePath = 'csvimport/output_part_1.csv';
+  
+  try {
+      console.log('finding file path');
+      await fs.promises.access(csvFilePath);
+
+      const results = [];
+
+      const csvStream = fs.createReadStream(csvFilePath).pipe(csvParser());
+
+      console.log('putting files into an array');
+      var count = 0;
+      for await (const record of csvStream) {
+          if(count % 1000 == 0){
+              console.log(count);
+          }
+          count = count + 1;
+
+          for (const key in record) {
+              if (record[key] === '') {
+                  record[key] = null;
+              }
+          }
+          await CentralNodeGameInformation.create(record);
+      }
+
+      console.log('CSV import completed successfully.');
+      res.sendStatus(200);
+
+  } catch(err) {
+      console.log('Error importing CSV: ', err);
+      res.sendStatus(500);
+  }
+});
+
+transactionsRouter.get('/importcsvnode2', async (req, res) => {
+  console.log('CSV NODE 2 IMPORT CALLED');
+  const csvFilePath = 'csvimport/output_part_1.csv';
+
+  try {
+      console.log('finding file path');
+      await fs.promises.access(csvFilePath);
+
+      const results = [];
+
+      const csvStream = fs.createReadStream(csvFilePath).pipe(csvParser());
+
+      console.log('putting files into an array');
+      var count = 0;
+      for await (const record of csvStream) {
+          if(count % 1000 == 0){
+              console.log(count);
+          }
+          count = count + 1;
+
+          for (const key in record) {
+              if (record[key] === '') {
+                  record[key] = null;
+              }
+          }
+          
+          if (record.Releasedate) {
+            const parsedDate = parse(record.Releasedate, 'dd-MMM-yy', new Date());
+            const releaseYear = parsedDate.getFullYear();
+            
+            if (releaseYear < 2010) {
+              await Node2GameInformation.create(record); // Insert into Node3GameInformation
+            }
+          }
+        }
+
+      console.log('CSV import completed successfully.');
+      res.sendStatus(200);
+
+  } catch(err) {
+      console.log('Error importing CSV: ', err);
+      res.sendStatus(500);
+  }
+});
+
+transactionsRouter.get('/importcsvnode3', async (req, res) => {
+  console.log('CSV NODE 3 IMPORT CALLED');
+  const csvFilePath = 'csvimport/output_part_1.csv';
+
+  try {
+      console.log('finding file path');
+      await fs.promises.access(csvFilePath);
+
+      const results = [];
+
+      const csvStream = fs.createReadStream(csvFilePath).pipe(csvParser());
+
+      console.log('putting files into an array');
+      var count = 0;
+      for await (const record of csvStream) {
+          if(count % 1000 == 0){
+              console.log(count);
+          }
+          count = count + 1;
+
+          for (const key in record) {
+              if (record[key] === '') {
+                  record[key] = null;
+              }
+          }
+          
+          if (record.Releasedate) {
+            const parsedDate = parse(record.Releasedate, 'dd-MMM-yy', new Date());
+            const releaseYear = parsedDate.getFullYear();
+            
+            if (releaseYear >= 2010) {
+              await Node3GameInformation.create(record); // Insert into Node3GameInformation
+            }
+          }
+        }
+    
+      console.log('CSV import completed successfully.');
+      res.sendStatus(200);
+
+  } catch(err) {
+      console.log('Error importing CSV: ', err);
+      res.sendStatus(500);
+  }
+});*/
 
 export default transactionsRouter;
